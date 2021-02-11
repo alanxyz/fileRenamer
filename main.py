70f7880e38aa1c7894e1f536ea7f1ff4540a1e6a
Alan <alan@alnn.xyz>
2021-02-11 21:02:33 +0000

    simplify it a little

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ print("This will sort your images which begin with IMG with tags alphabetically.
 
 for x in os.listdir(path):
     if x.endswith(('.jpg', '.png')) and x.startswith("IMG"):
+
         imageDir = path + pathSeparator + x
-        imageExtension = x[-4:]
+        imageExtension = x[-4:] # this might not work with other file extensions but it works lol
+
         print(imageDir)
+
         userInput = input("Input some tags for this image: ")
-        userInputSplit = userInput.split(" ")
-        userInputSplitSorted = sorted(userInputSplit)
-        newName = '_'.join(userInputSplitSorted)
-        newFileName = newName + "_" + str(random.randint(1, 100))
+        userInputSplitSorted = sorted(userInput.split(" "))
+        newFileName = '_'.join(userInputSplitSorted) + "_" + str(random.randint(1, 100))
         newImageDir = path + pathSeparator + newFileName + imageExtension
+
         os.rename(imageDir, newImageDir)
-        print('\n')
