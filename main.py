a722cf285c2ab9868aa85e1819ce2cf66a7d63b0
Alan <alan@alnn.xyz>
2021-02-11 20:48:16 +0000

    simplify some code - credit to hipoza

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ pathSeparator = '//'
 print("This will sort your images which begin with IMG with tags alphabetically.\n")
 
 for x in os.listdir(path):
-    if x.endswith(('.jpg', '.png')):
-        if x.startswith("IMG"):
-            imageDir = path + pathSeparator + x
-            imageExtension = x[-4:]
-            print(imageDir)
-            userInput = input("Input some tags for this image: ")
-            userInputSplit = userInput.split(" ")
-            userInputSplitSorted = sorted(userInputSplit)
-            newName = '_'.join(userInputSplitSorted)
-            newFileName = newName + "_" + str(random.randint(1, 100))
-            newImageDir = path + pathSeparator + newFileName + imageExtension
-            os.rename(imageDir, newImageDir)
-            print('\n')
+    if x.endswith(('.jpg', '.png')) and x.startswith("IMG"):
+        imageDir = path + pathSeparator + x
+        imageExtension = x[-4:]
+        print(imageDir)
+        userInput = input("Input some tags for this image: ")
+        userInputSplit = userInput.split(" ")
+        userInputSplitSorted = sorted(userInputSplit)
+        newName = '_'.join(userInputSplitSorted)
+        newFileName = newName + "_" + str(random.randint(1, 100))
+        newImageDir = path + pathSeparator + newFileName + imageExtension
+        os.rename(imageDir, newImageDir)
+        print('\n')
